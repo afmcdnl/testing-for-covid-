42b38d04c884cc01f13454e7f31eabede3aae960
Andrew <afmcdnl@gmail.com>
2020-11-10 19:32:45 -0500

    Added a if statement

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 basic.forever(function () {
     music.playMelody("- - - - - - - - ", 120)
     while (true) {
-    	
+        if (true) {
+        	
+        } else {
+        	
+        }
     }
 })
